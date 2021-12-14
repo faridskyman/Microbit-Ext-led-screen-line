0bb5700caefd53e796956da3f4a4e67a63b323a7
Farid Mohd Ismail <farid.skyman@gmail.com>
2021-12-14 12:22:27 +0800

    Update main.ts, test.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
-})
+namespace screenMagic{
+    export function plotAt(index: number) :void {
+        const x = Math.floor(index / 5)
+        const y = Math.floor(index % 5)
+        led.plot(x, y);
+    }
+
+}
