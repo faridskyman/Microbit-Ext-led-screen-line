b33e932f37c8bffb58a6eeda230f6f8070bc4c20
Farid Mohd Ismail <farid.skyman@gmail.com>
2021-12-14 12:32:41 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 namespace screenMagic{
+
+/**
+ * Address LEDs Linearly row first
+ */
+//% blockId=screenmagicplotat
+//% block="plot at index $" 
+//% index.min=0 index.max=25
+
+
     export function plotAt(index: number) :void {
         const x = Math.floor(index / 5)
         const y = Math.floor(index % 5)
